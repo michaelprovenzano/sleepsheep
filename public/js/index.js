da722d4e739dc930ff2dc48fc3bd589cf96b3c90
Michael Provenzano <mikepmusic@gmail.com>
2020-04-24 13:17:25 -0400

    fixing the date formate so it is hopefully compatible with more devices

--- public/js/index.js
+++ public/js/index.js
@@ -354,7 +354,7 @@ function formatDate(date) {
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day;
 
-  return [year, month, day].join('-');
+  return [year, month, day].join('/');
 }
 
 function formatTime(date) {
@@ -370,5 +370,5 @@ function formatTime(date) {
     minutes = '0' + minutes;
   }
 
-  return `${hours}:${minutes}`;
+  return `${hours}:${minutes}:00`;
 }
